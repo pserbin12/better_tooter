7f4f85feaadd23053438ecf61c9806dc820f7b81
Joe OBrien <joe@objo.com>
2017-11-29 19:27:06 -0500

    .

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     "#{ENV['AWS_ACCESS_KEY_ID']}",      # required
     aws_secret_access_key: "#{ENV['AWS_SECRET_ACCESS_KEY']}",  # required
-    endpoint:              'https://s3.us-east-2.amazonaws.com' # optional, defaults to nil
+    endpoint:              'https://s3.us-east-2.amazonaws.com', # optional, defaults to nil
     region:                "#{ENV['S3_REGION']}"
   }
   config.fog_directory  = 'name_of_directory'                          # required
