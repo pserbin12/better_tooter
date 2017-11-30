edd89c3bbefce56f8e8c29be399c35108d47ab9a
Joe OBrien <joe@objo.com>
2017-11-29 19:19:01 -0500

    adding fog-aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     "#{ENV['AWS_ACCESS_KEY_ID']}",      # required
     aws_secret_access_key: "#{ENV['AWS_SECRET_ACCESS_KEY']}",  # required
-    host:                  's3.example.com',             # optional, defaults to nil
   }
+  
   config.fog_directory  = "#{ENV['S3_BUCKET_NAME']}"          # required
   config.fog_public     = false                                        # optional, defaults to true
   config.cache_dir     = "#{Rails.root}/tmp/uploads"   # For Heroku
